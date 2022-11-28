d78a4432a665186c61b45cc30822f5e5574fad82
Kelvin Anigboro <rukywe@me.com>
2022-11-28 21:45:06 +0000

    add verify token function and protected route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const createError = require("http-errors");
 const connectDB = require("./utils/connect_to_db");
+const { verifyToken } = require("./utils/jwt_helper");
 
 const AuthRoute = require("./Routes/Auth.route");
 
@@ -13,8 +14,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res, next) => {
-  res.send("Hello world");
+app.get("/", verifyToken, async (req, res, next) => {
+  res.send("Hello world from express");
 });
 
 app.use("/auth", AuthRoute);
