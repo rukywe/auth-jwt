d3351b57907fb759b3c5b3dc313456ac70360a52
Kelvin Anigboro <rukywe@me.com>
2022-11-28 21:56:22 +0000

    handle JWT errors correctly

--- utils/jwt_helper.js
+++ utils/jwt_helper.js
@@ -8,7 +8,7 @@ module.exports = {
 
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "15secs",
         issuer: "randomsite.com",
         audience: userId
       };
@@ -31,7 +31,9 @@ module.exports = {
 
     JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) {
-        return next(createError.Unauthorized());
+        const message =
+          err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+        return next(createError.Unauthorized(message));
       }
       req.payload = payload;
       next();
